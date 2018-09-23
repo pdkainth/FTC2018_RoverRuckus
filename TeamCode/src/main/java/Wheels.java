cc35233a7785a5f178c559d4446c5b93baeeffef
pdkainth <pdkainth@gmail.com>
2018-09-22 17:47:57 -0700

    Created a wheels class to calculate and provide power to the mecanum wheels Modified the teleOp to accomodate fo the wheels class

---
+++ TeamCode/src/main/java/Wheels.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Wheels {
+
+    /**
+     * Motor 3- front left
+     * Motor2 - front right
+     * Motor 1 - back left
+     * Motor 4 - back right
+     */
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+
+    public void init(HardwareMap hardwareMap){
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "Motor3");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "Motor2");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "Motor1");
+        backRightDrive = hardwareMap.get(DcMotor.class, "Motor0");
+
+        frontLeftDrive.setPower(0.0);
+        frontRightDrive.setPower(0.0);
+        backLeftDrive.setPower(0.0);
+        backRightDrive.setPower(0.0);
+
+        frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+    }
+
+    public void drive(double drive, double strafe, double rotate, Telemetry telemetry){
+
+        double frontLeftPower = drive + strafe + rotate;
+        double backLeftPower = drive - strafe + rotate;
+        double frontRightPower = drive - strafe - rotate;
+        double backRightPower = drive + strafe - rotate;
+
+        frontLeftDrive.setPower(frontLeftPower);
+        frontRightDrive.setPower(frontRightPower);
+        backLeftDrive.setPower(backLeftPower);
+        backRightDrive.setPower(backRightPower);
+
+        telemetry.addData("Wheel","FL %.2f FR %.2f BL %.2f BR %.2f", frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+    }
+
+    public void stop(){
+        frontRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+    }
+}
--- TeamCode/src/main/java/myTeleOp.java
+++ TeamCode/src/main/java/myTeleOp.java
@@ -1,46 +1,69 @@
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.Hardware;
 
-@TeleOp(name="Testing teleOP", group="Linear Opmode")
+import org.firstinspires.ftc.teamcode.Wheels;
 
+@TeleOp(name="Testing teleOP")
 
-public class myTeleOp extends LinearOpMode {
+public class myTeleOp extends OpMode {
 
   private ElapsedTime runtime = new ElapsedTime();
-  private DcMotor leftDrive = null;
+  //private DcMotor leftDrive = null;
+  private Wheels mecanum = new Wheels();
+
 
   @Override
-  public void runOpMode(){
+  public void init(){
+    mecanum.init(hardwareMap);
+  }
+
+  @Override
+  public void init_loop(){
+
+  }
+
+
+
+  @Override
+  public void start(){
+    runtime.reset();
+  }
+
+
+
+  @Override
+  public void loop(){
 
     telemetry.addData("Status", "Initialized");
-    telemetry.update();
-
-    leftDrive  = hardwareMap.get(DcMotor.class, "motorTest");
-    leftDrive.setDirection(DcMotor.Direction.FORWARD);
-
-    waitForStart();
-    runtime.reset();
-
-    while (opModeIsActive()){
-
-      double leftPower;
-
-      double drive = -gamepad1.left_stick_y;
-      double turn  =  gamepad1.right_stick_x;
-
-      leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-
-      leftDrive.setPower(leftPower);
-
-      telemetry.addData("Status", "Run Time: " + runtime.toString());
-      telemetry.addData("Motors", "motorTest (%.2f)", leftPower);
-      telemetry.update();
+    updateWheels();
 
     }
 
+  @Override
+  public void stop(){
+    mecanum.stop();
   }
+
+
+  public void updateWheels(){
+
+
+    double drive = -gamepad1.left_stick_y;
+    double strafe = 0.0;
+
+    if(gamepad1.dpad_left == true){
+      strafe = -1.0;
+    } else if(gamepad1.dpad_right == true){
+      strafe = 1.0;
+    }
+
+    double rotate = gamepad1.right_stick_x;
+
+    mecanum.drive(drive, strafe, rotate, telemetry);
+    telemetry.addData("Wheel","Wheel power: D: %.2f S: %.2f R: %.2f", drive, strafe, rotate);
+  }
+
 }
