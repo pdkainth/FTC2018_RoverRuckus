06ae462cadab998c0afa09bea2d7f7f9b5ad9559
Prabhdeep Kainth <pdkainth@gmail.com>
2018-12-24 16:35:01 -0800

    moved route info to autoroute for blue and red alliance updated data for autonomous run based on testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive_BLUE_DEPOT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive_BLUE_DEPOT.java
@@ -9,23 +9,6 @@ public class AutoDrive_BLUE_DEPOT extends MyAutoDrive {
   public void init() {
     setAlliance(AllianceColor.BLUE, AllianceSide.DEPOT);
     super.init();
-
-    autoRoute.landingPos.setPos(-18.0f, 14.0f, -135.0f);
-    autoRoute.mineralPos[0].setPos(-16.0f, 44.0f, 180.0f); // Right
-    autoRoute.mineralPos[1].setPos(-30.0f, 22.0f, 90.0f);  // Center
-    autoRoute.mineralPos[2].setPos(-38.5f, 13.5f, 90.0f);  // Left
-
-    autoRoute.mineralMoveDistance[0] = 10;
-    autoRoute.mineralMoveDistance[1] = 10;
-    autoRoute.mineralMoveDistance[2] = 10;
-
-    autoRoute.depotRoutePos[0].setPos(-40.0f, 12.0f, -135.0f);
-    autoRoute.depotRoutePos[1].setPos(-60.0f, 12.0f, 90.0f);
-    autoRoute.depotRoutePos[2].setPos(-60.0f, 60.0f, -90.0f);
-
-    autoRoute.craterPos.setPos(-60.0f, -30.0f, -90.0f);
-
-    autoRoute.vuforiaAngle = 160.0f;
   }
 
   public void start(){
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRoute.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRoute.java
@@ -9,9 +9,13 @@ public class AutoRoute {
   Position landingPos = new Position();
   Position[] mineralPos = new Position[NUM_MINERAL_POS];
   float[] mineralMoveDistance = new float[NUM_MINERAL_POS];
-  Position[] depotRoutePos = new Position[NUM_DEPOT_ROUTE_POS];
+  float[] mineralMoveBackDistance = new float[NUM_MINERAL_POS];
+  int[] numDepotRoutePos = {0, 0, 0};
+  Position[][] depotRoutePos = new Position[NUM_MINERAL_POS][NUM_DEPOT_ROUTE_POS];
   Position craterPos = new Position();
 
+  float preVuforiaDrive;
+  float preVuforiaAngle;
   float vuforiaAngle;
 
   public AutoRoute()
@@ -20,9 +24,78 @@ public class AutoRoute {
       mineralPos[index] = new Position();
     }
     for (int index = 0; index < NUM_DEPOT_ROUTE_POS; index++){
-      depotRoutePos[index] = new Position();
+      for (int index1 = 0; index1 < NUM_DEPOT_ROUTE_POS; index1++) {
+        depotRoutePos[index][index1] = new Position();
+      }
     }
-
   }
+
+  public void setDepotRoutePos() {
+    landingPos.setPos(-18.0f, 14.0f, -135.0f);
+    mineralPos[0].setPos(-18.0f, 44.0f, 160.0f); // Right
+    mineralPos[1].setPos(-26.0f, 26.0f, 135.0f);  // Center
+    mineralPos[2].setPos(-46.0f, 12.0f, 90.0f);  // Left
+
+    mineralMoveDistance[0] = 10.0f;
+    mineralMoveDistance[1] = 10.0f;
+    mineralMoveDistance[2] = 10.0f;
+
+    mineralMoveBackDistance[0] = 0.0f;
+    mineralMoveBackDistance[1] = 0.0f;
+    mineralMoveBackDistance[2] = 0.0f;
+
+    numDepotRoutePos[0] = 1;
+    depotRoutePos[0][0].setPos(-54.0f, 50.0f, -45.0f);
+
+    numDepotRoutePos[1] = 1;
+    depotRoutePos[1][0].setPos(-54.0f, 42.0f, -45.0f);
+
+    numDepotRoutePos[2] = 1;
+    depotRoutePos[2][0].setPos(-54.0f, 42.0f, -45.0f);
+
+    craterPos.setPos(-60.0f, -30.0f, -90.0f);
+
+    preVuforiaDrive = 0.0f;
+    preVuforiaAngle = 160.0f;
+    vuforiaAngle = 160.0f;
+  }
+
+  public void setCraterRoutePos() {
+    landingPos.setPos(14.0f, 18.0f, 135.0f);
+    mineralPos[2].setPos(18.0f, 42.0f, 45.0f); // Right
+    mineralPos[1].setPos(27.0f, 27.0f, 45.0f);  // Center
+    mineralPos[0].setPos(40.0f, 20.0f, 45.0f);  // Left
+
+    mineralMoveDistance[0] = 20.0f;
+    mineralMoveDistance[1] = 20.0f;
+    mineralMoveDistance[2] = 20.0f;
+
+    mineralMoveBackDistance[0] = 0.0f;
+    mineralMoveBackDistance[1] = 0.0f;
+    mineralMoveBackDistance[2] = 0.0f;
+
+    numDepotRoutePos[0] = 0;
+    //depotRoutePos[0][0].setPos(6.0f, 50.0f, 180.0f);
+    //depotRoutePos[0][1].setPos(60.0f, 12.0f, 90.0f);
+    //depotRoutePos[0][1].setPos(-54.0f, 54.0f, -45.0f);
+
+    numDepotRoutePos[1] = 0;
+    //depotRoutePos[1][0].setPos(6.0f, 48.0f, 135.0f);
+    //depotRoutePos[1][1].setPos(60.0f, 12.0f, 90.0f);
+    //depotRoutePos[1][2].setPos(-60.0f, 60.0f, -45.0f);
+
+    numDepotRoutePos[2] = 0;
+    //depotRoutePos[2][0].setPos(6.0f, 48.0f, 135.0f);
+    //depotRoutePos[2][1].setPos(60.0f, 12.0f, 90.0f);
+    //depotRoutePos[2][2].setPos(-60.0f, 60.0f, -45.0f);
+
+
+    //craterPos.setPos(-60.0f, -30.0f, -90.0f);
+
+    preVuforiaDrive = 6.0f;
+    preVuforiaAngle = 90.0f;
+    vuforiaAngle = -170.0f;
+  }
+
 }
 
