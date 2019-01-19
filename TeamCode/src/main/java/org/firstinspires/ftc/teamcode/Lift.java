3c89dd35d12bf2bdbc24d7223e00d9d775be5baa
Prabhdeep Kainth <pdkainth@gmail.com>
2019-01-18 19:02:59 -0800

    Final adjustment for robot lift and phone tilt based on testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Lift extends DcMotorUpDn {
 
-  public static final int LIFT_ENC_POS_HANG = 15000;
+  public static final int LIFT_ENC_POS_HANG = 15500;
   public static final int LIFT_ENC_POS_MIN = 0;
 
   public void init(HardwareMap hardwareMap) {
